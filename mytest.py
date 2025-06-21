4bc57537538539c4d8c3b60706db3218e0fdf3b1
kenkenken0916 <supercalifragilistichsu@gmail.com>
2025-06-22 02:05:33 +0800

    Update mytest.py

--- mytest.py
+++ mytest.py
@@ -35,49 +35,42 @@ def gohome():
     pyautogui.press('enter')
 
 
-def func():
-    t=100 #66.4
-    repeat=3
-    for o in range(0,2):
+
+
+def func(delayhour=4, wait=False):
+    delaytime= delayhour* 6
+    if wait:
+        print("Waiting for", delaytime, "hours")
+        for i in range(0,delaytime):
+            time.sleep(600)
+            pyautogui.keyDown('w')
+            time.sleep(.5)
+            pyautogui.keyUp('w')
+            pyautogui.keyDown('s')
+            time.sleep(.5)
+            pyautogui.keyUp('s')
+
+        pyautogui.click(x=746, y=200)
+        pyautogui.click(x=746, y=200)
+        time.sleep(30)
+        tycode('/skyblock')
+        time.sleep(20)
         gohome()
-        #winsound.Beep(1000,1000)
-        time.sleep(.5)
-        pyautogui.mouseDown(button='left',x=0,y=0)
-        for i in range(0,9):
-            pyautogui.keyDown('w')
-            time.sleep(1.5)
-            pyautogui.keyUp('w')
-            pyautogui.keyDown('d')
-            time.sleep(18.5)
-            #time.sleep(random.random())
-            pyautogui.keyUp('d')
-            pyautogui.keyDown('w')
+        time.sleep(10)
 
-            time.sleep(1.5)
-            pyautogui.keyUp('w')
-            pyautogui.keyDown('a')
-            time.sleep(18.5)
-            #time.sleep(random.random())
-            pyautogui.keyUp('a')
-        pyautogui.mouseUp(button='left',x=0,y=0)
-    
-    # pyautogui.press('esc')
-    # pyautogui.click(x=950,y=550)
-    # pyautogui.click(x=950,y=550)
-    # os.system("shutdown -h now")
-
-#
-def func2():
     for i in range (0,5):
-        print('f'+str(i))
+        print('runing'+str(i))
         
         mepu()
         time.sleep(2)
+
         pyautogui.press('esc')
         pyautogui.click(x=950,y=550)
         pyautogui.click(x=950,y=550)
 
-        rep=random.randint(18,24)
+        print("start waiting")
+
+        rep=random.randint(delaytime-3, delaytime+3)
         for i in range(0,rep):
             time.sleep(600)
             pyautogui.keyDown('w')
@@ -86,6 +79,8 @@ def func2():
             pyautogui.keyDown('s')
             time.sleep(.5)
             pyautogui.keyUp('s')
+
+        print("going back to game")
         
         pyautogui.click(x=746, y=200)
         pyautogui.click(x=746, y=200)
@@ -98,23 +93,20 @@ def func2():
 
 
 
-
-#Mouse Position: (x=1490, y=220)
-#Mouse Position: (x=1919, y=820)
-
-def mepu():
+def mepu(clean=True)->None:
     t=67
     pyautogui.keyDown('shiftleft')
     time.sleep(.1)
     pyautogui.keyUp('shiftleft')
     for i in range (15):
         print('m'+str(i))
-        if(i%4==3):
+        if(i%4==3 and clean):
             pyautogui.press('4')
             time.sleep(1)
             pestout()
             time.sleep(1)
             pyautogui.press('3')
+            #continue
         
         gohome()
         time.sleep(.5)
@@ -123,18 +115,14 @@ def mepu():
         for n in range(0,6):
             pyautogui.keyDown('a')
             time.sleep(t)
-            #time.sleep(random.random())
+            #delay?
             pyautogui.keyUp('a')
             pyautogui.keyDown('d')
             time.sleep(t)
-            #time.sleep(random.random())
+            #delay?
             pyautogui.keyUp('d')
     pyautogui.keyUp('w')
     pyautogui.mouseUp(button='left',x=0,y=0)
-    # pyautogui.press('esc')
-    # pyautogui.click(x=950,y=550)
-    # pyautogui.click(x=950,y=550)
-    # os.system("shutdown -h now")
 
 def mepush():
     mepu()
@@ -147,27 +135,32 @@ def mepush():
 def pestout():
     gohome()
     time.sleep(2)
-    pyautogui.mouseDown(button='right',x=0,y=0)
+    
     for n in range(0,6):
-        if(n==1 or n==4):
-            pyautogui.keyDown('a')
-            pyautogui.keyDown('shiftleft')
-            time.sleep(163)
-            pyautogui.keyUp('a')
-            pyautogui.keyUp('shiftleft')
-        else:
-            pyautogui.keyDown('a')
-            time.sleep(50)
-            pyautogui.keyUp('a')
+        pyautogui.mouseDown(button='right',x=0,y=0)
+        # if(n==1 or n==4):
+        #     pyautogui.keyDown('a')
+        #     pyautogui.keyDown('shiftleft')
+        #     time.sleep(159)
+        #     pyautogui.keyUp('a')
+        #     pyautogui.keyUp('shiftleft')
+        # else:
+        pyautogui.keyDown('a')
+        time.sleep(49)
+        pyautogui.keyUp('a')
+
+        pyautogui.mouseUp(button='right',x=0,y=0)
 
         pyautogui.keyDown('w')
         time.sleep(1)
         pyautogui.keyUp('w')
 
-        if(n==2 or n==5):
+        pyautogui.mouseDown(button='right',x=0,y=0)
+
+        if(n==1 or n==3 or n==5):
             pyautogui.keyDown('d')
             pyautogui.keyDown('shiftleft')
-            time.sleep(163)
+            time.sleep(159)
             pyautogui.keyUp('d')
             pyautogui.keyUp('shiftleft')
         else:
@@ -179,7 +172,8 @@ def pestout():
         time.sleep(1)
         pyautogui.keyUp('w')
 
-    pyautogui.mouseUp(button='right',x=0,y=0)
+        pyautogui.mouseUp(button='right',x=0,y=0)
+    
     print("clean")
 
 def coco()->None:#spd我忘了ㄏ 仰角45
@@ -219,7 +213,7 @@ def sugar()->None:#spd328 仰角~0
         pyautogui.keyUp('s')
     pyautogui.mouseUp(button='left',x=0,y=0)
 
-def cati()->None:#spd400 仰角~0
+def cati()->None:#spd500 仰角~0
     t=18.5
     for i in range(0,5):
         gohome()
@@ -259,7 +253,7 @@ def mush()->None:#spd232 仰角~5.5
         pyautogui.mouseUp(button='left',x=0,y=0)
     #winsound.Beep(1000,1000)
 
-def fino():#300
+def fino():#spd300
     t=89
     pyautogui.keyDown('shiftleft')
     time.sleep(.1)
@@ -280,55 +274,7 @@ def fino():#300
     pyautogui.mouseUp(button='left',x=0,y=0)
 
 
-def flyaround():
-    #gohome()
-    time.sleep(1)
-    pyautogui.press('9')
-    time.sleep(1)
-    pyautogui.click(x=0, y=0, button='right')
-    time.sleep(1)
-    pyautogui.click(x=749, y=283)
-    time.sleep(1)
-    pyautogui.click(x=749, y=354)
-    time.sleep(1)
-    pyautogui.click(x=963, y=433, button='right')
-    time.sleep(2)
-    pyautogui.keyDown(' ')
-    pyautogui.keyUp(' ')
-    pyautogui.keyDown(' ')
-    time.sleep(0.5)
-    pyautogui.keyDown('w')
-    time.sleep(1)
-    pyautogui.keyUp('w')
-    time.sleep(0.5)
-    pyautogui.press('8')
-    time.sleep(0.5)
-    pyautogui.click(x=0, y=0)
-    time.sleep(0.5)
-    pyautogui.press('4')
-    pyautogui.mouseDown(button='right',x=0,y=0)
-    pyautogui.keyDown('a')
-    time.sleep(30)
-    pyautogui.keyUp('a')
-    for i in range (0,9):
-        pyautogui.keyDown('w')
-        time.sleep(2)
-        pyautogui.keyUp('w')
-        pyautogui.keyDown('d')
-        time.sleep(45)
-        pyautogui.keyUp('d')
-        pyautogui.keyDown('w')
-        time.sleep(2)
-        pyautogui.keyUp('w')
-        pyautogui.keyDown('a')
-        time.sleep(45)
-        pyautogui.keyUp('a')
-    pyautogui.mouseUp(button='right',x=0,y=0)
-    pyautogui.keyDown('shiftleft')
-    time.sleep(5)
-    pyautogui.keyUp('shiftleft')
-    pyautogui.press('3')
 
 # Set up the hotkeys
-keyboard.add_hotkey('u+i',func2)#func2
+keyboard.add_hotkey('u+i',func)#func2
 keyboard.wait('esc')
